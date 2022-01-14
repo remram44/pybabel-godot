8801a6e856b85dd1ec2f88c20342c5412e120c05
Remi Rampin <remi@rampin.org>
2022-01-14 12:59:55 -0500

    test: Add array, with multiline string

--- testproject/Custom.gd
+++ testproject/Custom.gd
@@ -3,6 +3,7 @@ extends Node2D
 export(int) var age
 export(String) var fullname
 export(String) var catchphrase
+export(Array, String, MULTILINE) var dialogue
 
 func _ready():
 	print(tr("Hello from script"))
