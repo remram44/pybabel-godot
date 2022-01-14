a2376c41d861dcd1032075bbced3e631b004b960
Remi Rampin <remi@rampin.org>
2022-01-14 11:43:38 -0500

    Add script to run test locally

---
+++ run_test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eu
+
+(cd testproject/ && pybabel extract -F babelrc -k Label/text -k Resource/catchphrase -k tr -o translations_new.pot .)
+
+sed -e '/POT-Creation-Date/d' -e 's/\b20[0-9][0-9]\b/2020/' -e 's/Babel [0-9.]\+/Babel 2.0/' <testproject/translations.pot >expected.pot
+sed -e '/POT-Creation-Date/d' -e 's/\b20[0-9][0-9]\b/2020/' -e 's/Babel [0-9.]\+/Babel 2.0/' <testproject/translations_new.pot >actual.pot
+diff -u expected.pot actual.pot
